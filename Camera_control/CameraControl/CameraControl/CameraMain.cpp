853b942cc3291a219f338cec9e547175ab7d03d4
성승남 <seungnam@seungnam.co.kr>
2015-05-08 21:40:44 +0900

    비동기식으로 2대 촬영 구현완료!

--- Camera_control/CameraControl/CameraControl/CameraMain.cpp
+++ Camera_control/CameraControl/CameraControl/CameraMain.cpp
@@ -1,63 +1,71 @@
 #include <iostream>
+#include <windows.h>
 #include "Header/EDSDK.h"
 
 using namespace std;
 
+void camera1_ready(EdsCameraRef& camera1)
+{
+
+}
+
+void camera2_ready(EdsCameraRef& camera2)
+{
+
+}
+
 int main()
 {
 	//EDSDK 초기 설정관련
-	EdsError err = EDS_ERR_OK;
+	EdsError err1 = EDS_ERR_OK;
+	EdsError err2 = EDS_ERR_OK;
 	EdsCameraRef camera = NULL;
 	EdsCameraRef camera2 = NULL;
 	EdsCameraListRef cameraList = NULL;
 	EdsUInt32 count = 0;
 	bool isSDKLoaded = false;
-
 	int input;
 
-	err = EdsInitializeSDK();
+	DWORD start;
+	DWORD end;
+
+	err1 = EdsInitializeSDK();
+
 
 	//카메라 불러오기
-	if (err == EDS_ERR_OK)
+	if (err1 == EDS_ERR_OK)
 	{
 		isSDKLoaded = true;
 
-		err = EdsGetCameraList(&cameraList); //카메라 리스트 받아오기
-		//카메라 갯수 받아오기
-		if (err = EDS_ERR_OK)
+		err1 = EdsGetCameraList(&cameraList);
+		if (err1 = EDS_ERR_OK)	//cameraList 성공
 		{
-			err = EdsGetChildCount(cameraList, &count);
+			err1 = EdsGetChildCount(cameraList, &count);
 			if (count == 0)
 			{
-				err = EDS_ERR_DEVICE_NOT_FOUND;
+				err1 = EDS_ERR_DEVICE_NOT_FOUND;
 			}
 		}
 
-		if (err == EDS_ERR_OK)
+		if (err1 == EDS_ERR_OK)
 		{
-			err = EdsGetChildAtIndex(cameraList, 0, &camera);
-			cout << "camera1 is on" << endl;
-
-				err = EdsGetChildAtIndex(cameraList, 1, &camera2);
+			err1 = EdsGetChildAtIndex(cameraList, 0, &camera);
+			err2 = EdsGetChildAtIndex(cameraList, 1, &camera2);
+			if (err1 == EDS_ERR_OK) cout << "camera1 is on" << endl;
+			if (err2 == EDS_ERR_OK) cout << "camera2 is on" << endl;
 		}
 
-		//release camera list
 		if (cameraList != NULL)
 		{
 			EdsRelease(cameraList);
 			cameraList = NULL;
 		}
 
-		//Set event handler
-
-		//Open session
-		if (err == EDS_ERR_OK)
+		//openSession
+		if (err1 == EDS_ERR_OK&&err2 == EDS_ERR_OK)
 		{
-			err = EdsOpenSession(camera);
-			if (count > 1)
-			{
-				err = EdsOpenSession(camera2);
-			}
+			err1 = EdsOpenSession(camera);
+			err2 = EdsOpenSession(camera2);
 		}
 
 		//영상촬영 준비
@@ -65,77 +73,73 @@ int main()
 		EdsUInt32 device2;
 		EdsUInt32 record_start = 4;
 		EdsUInt32 record_stop = 0;
-		err = EdsGetPropertyData(camera, kEdsPropID_Evf_OutputDevice, 0, sizeof(device), &device);
+		EdsUInt32 record_start2 = 4;
+		EdsUInt32 record_stop2 = 0;
+		err1 = EdsGetPropertyData(camera, kEdsPropID_Evf_OutputDevice, 0, sizeof(device), &device);
+		err2 = EdsGetPropertyData(camera2, kEdsPropID_Evf_OutputDevice, 0, sizeof(device2), &device2);
 
-		if (err == EDS_ERR_OK)
+		if (err1 == EDS_ERR_OK&&err2 == EDS_ERR_OK)
 		{
 			device |= kEdsEvfOutputDevice_TFT;
-			err = EdsSetPropertyData(camera, kEdsPropID_Evf_OutputDevice, 0, sizeof(device), &device);
+			device2 |= kEdsEvfOutputDevice_TFT;
+			err1 = EdsSetPropertyData(camera, kEdsPropID_Evf_OutputDevice, 0, sizeof(device), &device);
+			err2 = EdsSetPropertyData(camera2, kEdsPropID_Evf_OutputDevice, 0, sizeof(device2), &device2);
 		}
 
 		EdsUInt32 saveTo = kEdsSaveTo_Camera;
-		err = EdsSetPropertyData(camera, kEdsPropID_SaveTo, 0, sizeof(saveTo), &saveTo);
+		EdsUInt32 saveTo2 = kEdsSaveTo_Camera;
+		err1 = EdsSetPropertyData(camera, kEdsPropID_SaveTo, 0, sizeof(saveTo), &saveTo);
+		err2 = EdsSetPropertyData(camera2, kEdsPropID_SaveTo, 0, sizeof(saveTo2), &saveTo2);
 
-		if (count > 1)
-		{
-			err = EdsGetPropertyData(camera2, kEdsPropID_Evf_OutputDevice, 0, sizeof(device2), &device2);
-
-			if (err == EDS_ERR_OK)
-			{
-				device |= kEdsEvfOutputDevice_TFT;
-				err = EdsSetPropertyData(camera2, kEdsPropID_Evf_OutputDevice, 0, sizeof(device2), &device2);
-			}
-
-			EdsUInt32 saveTo = kEdsSaveTo_Camera;
-			err = EdsSetPropertyData(camera2, kEdsPropID_SaveTo, 0, sizeof(saveTo), &saveTo);
-		}
-
-		//do something
+		//command
 		while (1)
 		{
 			cin >> input;
-				//err = EdsSendCommand(camera, kEdsCameraCommand_TakePicture, 0);	//사진촬영 파트
 			if (input == 1)
 			{
-				err = EdsSetPropertyData(camera, kEdsPropID_Record, 0, sizeof(record_start), &record_start);
-				if (count>1)	err = EdsSetPropertyData(camera2, kEdsPropID_Record, 0, sizeof(record_start), &record_start);
+				start = GetTickCount();
+				err1 = EdsSetPropertyData(camera, kEdsPropID_Record, 0, sizeof(record_start), &record_start);
+				end = GetTickCount();
+				cout << end - start << endl;
+				start = GetTickCount();
+				err2 = EdsSetPropertyData(camera2, kEdsPropID_Record, 0, sizeof(record_start2), &record_start2);
+				end = GetTickCount();
+				cout << end - start << endl;
 			}
 			else if (input == 2)
 			{
-				err = EdsSetPropertyData(camera, kEdsPropID_Record, 0, sizeof(record_start), &record_stop);
-				if (count>1)	err = EdsSetPropertyData(camera2, kEdsPropID_Record, 0, sizeof(record_start), &record_stop);
+				err1 = EdsSetPropertyData(camera, kEdsPropID_Record, 0, sizeof(record_stop), &record_stop);
+				err2 = EdsSetPropertyData(camera2, kEdsPropID_Record, 0, sizeof(record_stop2), &record_stop2);
 			}
 			else if (input == 0)
 			{
-				err = EdsGetPropertyData(camera, kEdsPropID_Evf_OutputDevice, 0, sizeof(device), &device);
-				if (count>1)	err = EdsGetPropertyData(camera2, kEdsPropID_Evf_OutputDevice, 0, sizeof(device2), &device2);
+				err1 = EdsGetPropertyData(camera, kEdsPropID_Evf_OutputDevice, 0, sizeof(device), &device);
+				err2 = EdsGetPropertyData(camera2, kEdsPropID_Evf_OutputDevice, 0, sizeof(device2), &device2);
 				break;
 			}
-
 		}
-		
 
 		//close session
-		if (err == EDS_ERR_OK)
+		if (err1 == EDS_ERR_OK)
 		{
-			err = EdsCloseSession(camera);
-			if (count>1) err = EdsCloseSession(camera2);
+			err1 = EdsCloseSession(camera);
+			err2 = EdsCloseSession(camera2);
 		}
 
 		//release camera
 		if (camera != NULL)
 		{
 			EdsRelease(camera);
+		}
+		if (camera2 != NULL)
+		{
 			EdsRelease(camera2);
 		}
 
-		//Terminate SDK
-		if (isSDKLoaded)
+		if (isSDKLoaded)	//Terminate SDK
 		{
 			EdsTerminateSDK();
 		}
 	}
-
-
 	return 0;
 }
